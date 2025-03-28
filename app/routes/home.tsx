d0ddb48f9fe95878a26f69f9c224da3534ebe332
DmitriyAgapov <prweb2012@gmail.com>
2025-03-28 06:32:32 +0500

    init

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}:any) {
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
+}
+
+export default function Home() {
+	return <Welcome />;
+}
